cb4007aa19226b114519b5e6effc7c38cac65b50
wxhzsr <2235249935@qq.com>
2021-04-21 10:45:45 +0800

    类的封装

---
+++ src/Demo02.java
@@ -0,0 +1,6 @@
+/**
+ * @author zhangshenrui
+ * @date 2021/4/21 10:42
+ */
+public class Demo02 {
+}
---
+++ src/testDemo02.java
@@ -0,0 +1,6 @@
+/**
+ * @author zhangshenrui
+ * @date 2021/4/21 10:43
+ */
+public class testDemo02 {
+}
